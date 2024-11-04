510a2e806869a5ab1a36d50ae345364e5c0e5b5e
Nicolas Levée <nicolasl@padok.fr>
2024-11-04 09:17:27 +0100

    feat: add server.py and deps from original repo
    Implementation of  https://github.com/sarperavci/CloudflareBypassForScraping

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+# Use the official Ubuntu image as the base image
+FROM python:3-slim-bookworm
+
+# Set environment variables to avoid interactive prompts during build
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Install necessary packages for Xvfb and pyvirtualdisplay
+RUN apt-get update && \
+    apt-get install -y \
+    chromium \
+    gnupg \
+    ca-certificates \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    libxss1 \
+    libxtst6 \
+    libnss3 \
+    libatk-bridge2.0-0 \
+    libgtk-3-0 \
+    x11-apps \
+    fonts-liberation \
+    libappindicator3-1 \
+    libu2f-udev \
+    libvulkan1 \
+    libdrm2 \
+    xdg-utils \
+    xvfb \
+    && rm -rf /var/lib/apt/lists/*
+
+# Set up a working directory
+WORKDIR /app
+
+# Install Python dependencies
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy application files
+COPY . .
+
+# Expose the port for the FastAPI server
+EXPOSE 8000
+
+# Default command
+CMD ["python", "server.py"]
---
+++ server.py
@@ -0,0 +1,139 @@
+import re
+from urllib.parse import urlparse
+
+from CloudflareBypasser import CloudflareBypasser
+from DrissionPage import ChromiumPage, ChromiumOptions
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from pythonjsonlogger import jsonlogger
+import argparse
+import logging 
+
+logger = logging.root
+# formatter = jsonlogger.JsonFormatter()
+logHandler = logging.StreamHandler
+# logHandler.setFormatter(formatter)
+logger.handlers = [logHandler]
+
+app = FastAPI()
+log = False
+log_level = logging.INFO
+
+# Pydantic model for the response
+class ClientRequest(BaseModel):
+    cmd: str = None
+    cookies: list = None
+    maxTimeout: int = None
+    url: str = None
+    postData: str = None
+
+class Solution(BaseModel):
+    url: str = None
+    status: int = None
+    response: str = None
+    cookies: list = None
+    userAgent: str = None
+        
+class ClientResponse(BaseModel):
+    status: str = None
+    message: str = ""
+    version: str = "1.0.0"
+    solution: Solution = None
+
+
+# Function to check if the URL is safe
+def is_safe_url(url: str) -> bool:
+    parsed_url = urlparse(url)
+    ip_pattern = re.compile(
+        r"^(127\.0\.0\.1|localhost|0\.0\.0\.0|::1|10\.\d+\.\d+\.\d+|172\.1[6-9]\.\d+\.\d+|172\.2[0-9]\.\d+\.\d+|172\.3[0-1]\.\d+\.\d+|192\.168\.\d+\.\d+)$"
+    )
+    hostname = parsed_url.hostname
+    if (hostname and ip_pattern.match(hostname)) or parsed_url.scheme == "file":
+        return False
+    return True
+
+
+# Function to bypass Cloudflare protection
+def bypass_cloudflare(url: str, retries: int, log: bool) -> ChromiumPage:
+    logger.info("Configuring ChromiumPage", extra={'requestUrl': url})
+    options = ChromiumOptions()
+    options.set_paths(browser_path="/usr/bin/chromium-browser").headless(False).auto_port()
+    options.set_argument("--no-sandbox")  # Necessary for Docker
+    options.set_argument("--disable-gpu")  # Optional, helps in some cases
+
+    page = ChromiumPage(addr_or_opts=options)
+    try:
+        logger.debug("Fetch ChromiumPage", extra={'requestUrl': url})
+        page.listen.start(targets=url)
+        page.get(url)
+
+        logger.debug("Apply bypass to ChromiumPage", extra={'requestUrl': url})
+        cf_bypasser = CloudflareBypasser(page, retries, logger)
+        cf_bypasser.bypass()
+        return page
+    except Exception as e:
+        page.listen.stop()
+        page.quit()
+        raise e
+
+# Endpoint to get Solver response
+@app.post("/v1")
+async def get_solverr(request: ClientRequest):
+    from pyvirtualdisplay import Display
+    if not is_safe_url(request.url):
+        raise HTTPException(status_code=400, detail="Invalid URL")
+    try:
+        if request.cmd == "request.get":
+            logger.info("Trying to solve", extra={'requestUrl': request.url})
+
+            # Start Xvfb for Docker
+            logger.debug("Start VirtualDisplay", extra={'requestUrl': request.url})
+            display = Display(visible=0, size=(1920, 1080))
+            display.start()
+
+            # Start bypass
+            logger.debug("Start ByPassing", extra={'requestUrl': request.url})
+            page = bypass_cloudflare(request.url, 5, log)
+            packet = page.listen.wait()
+            page.listen.stop()
+            cookies = page.cookies(as_dict=False)
+
+            # Build response
+            res = ClientResponse()
+            res.status = "ok"
+            res.solution = Solution(
+                url = packet.response.url,
+                status = packet.response.status,
+                response = page.html,
+                userAgent = page.user_agent,
+                cookies = cookies,
+            )
+
+            logger.debug("Closing ChromiumPage and VirtualDisplay", extra={
+                'requestUrl': request.url, 
+                'response_url': packet.response.url
+            })
+            page.quit()
+            display.stop()  # Stop Xvfb
+
+            return res
+    except Exception as e:
+        logger.error("An error occured while solving with error: %s", str(e), stack_info=True, extra={'requestUrl': request.url})
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# Main entry point
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Cloudflare bypass api")
+    parser.add_argument("--nolog", action="store_true", help="Disable logging")
+    parser.add_argument("--port", action="store_true", help="Port to bind service", default=8000)
+
+    args = parser.parse_args()
+    if not args.nolog:
+        log = True
+        log_level = logging.DEBUG
+    logger.setLevel(log_level)
+
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=args.port, log_config=None)
